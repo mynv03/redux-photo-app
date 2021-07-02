5325fb1e76cd05319ff6dd6a3b825bb1ac812fac
mynv <mynvse3133@gmail.com>
2021-07-02 18:24:25 +0700

    update

---
+++ src/components/custom-fields/InputField/index.jsx
@@ -0,0 +1,44 @@
+import PropTypes from "prop-types";
+import React from "react";
+import { FormGroup, Input, Label } from "reactstrap";
+
+InputField.propTypes = {
+  field: PropTypes.object.isRequired,
+  form: PropTypes.object.isRequired,
+
+  type: PropTypes.string,
+  label: PropTypes.string,
+  placeholder: PropTypes.string,
+  disabled: PropTypes.bool,
+};
+
+InputField.defaultProps = {
+  type: "text",
+  label: "",
+  placeholder: "",
+  disabled: false,
+};
+
+function InputField(props) {
+    const {
+        field, form,
+        type, label, placeholder, disabled
+    } = props
+
+    const { name} = field
+  return (
+    <FormGroup>
+        {label && <Label for={name}>{label}</Label>}
+      
+      <Input
+        id={name} 
+        {...field} 
+        placeholder={placeholder}
+        type={type}
+        disabled={disabled} 
+      />
+    </FormGroup>
+  );
+}
+
+export default InputField;
--- src/features/Photo/components/PhotoForm/index.jsx
+++ src/features/Photo/components/PhotoForm/index.jsx
@@ -1,51 +1,73 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-import Select from 'react-select';
-import { Button, Form, FormGroup, Input, Label } from 'reactstrap';
-import { PHOTO_CATEGORY_OPTIONS } from '../../../../constants/global';
-import Images from '../../../../constants/images';
-
+import { PHOTO_CATEGORY_OPTIONS } from "constants/global";
+import PropTypes from "prop-types";
+import React from "react";
+import Select from "react-select";
+import { Button, FormGroup, Input, Label } from "reactstrap";
+import Images from "constants/images";
+import { Formik, Form, FastField } from "formik";
+import InputField from "components/custom-fields/InputField";
+import SelectField from "components/custom-fields/selectField";
 PhotoForm.propTypes = {
   onSubmit: PropTypes.func,
 };
 
 PhotoForm.defaultProps = {
   onSubmit: null,
-}
+};
 
 function PhotoForm(props) {
   // npm i --save react-select
+  const initialValues = {
+    title: "",
+    categoryId: null,
+  };
   return (
-    <Form>
-      <FormGroup>
-        <Label for="titleId">Title</Label>
-        <Input name="title" id="titleId" placeholder="Eg: Wow nature ..." />
-      </FormGroup>
+    <Formik initialValues={initialValues}>
+      {(formikProps) => {
+        const { values, errors, touched } = formikProps;
+        console.log({ values, errors, touched });
+        return (
+          <Form>
+            <FastField
+              name="title"
+              component={InputField}
+              label="Title"
+              placeholder="Eg: Wow nature ..."
+            />
 
-      <FormGroup>
-        <Label for="categoryId">Category</Label>
-        <Select
-          id="categoryId"
-          name="categoryId"
+            <FastField
+              name="categoryId"
+              component={SelectField}
+              label="Category"
+              placeholder="What's your photo category?"
+              options={PHOTO_CATEGORY_OPTIONS}
+            />
 
-          placeholder="What's your photo category?"
-          options={PHOTO_CATEGORY_OPTIONS}
-        />
-      </FormGroup>
+            <FormGroup>
+              <Label for="categoryId">Photo</Label>
 
-      <FormGroup>
-        <Label for="categoryId">Photo</Label>
+              <div>
+                <Button type="button" outline color="primary">
+                  Random a photo
+                </Button>
+              </div>
+              <div>
+                <img
+                  width="200px"
+                  height="200px"
+                  src={Images.COLORFUL_BG}
+                  alt="colorful background"
+                />
+              </div>
+            </FormGroup>
 
-        <div><Button type="button" outline color="primary">Random a photo</Button></div>
-        <div>
-          <img width="200px" height="200px" src={Images.COLORFUL_BG} alt="colorful background" />
-        </div>
-      </FormGroup>
-
-      <FormGroup>
-        <Button color="primary">Add to album</Button>
-      </FormGroup>
-    </Form>
+            <FormGroup>
+              <Button color="primary">Add to album</Button>
+            </FormGroup>
+          </Form>
+        );
+      }}
+    </Formik>
   );
 }
 
