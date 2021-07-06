dd6b5c67f56df8e8cbc1e88ee07ac50fe68116b5
Tru Phuong Long <tru.phuonglong@petronas.com>
2021-07-06 20:46:28 +0700

    update

--- src/features/Photo/pages/Main/index.jsx
+++ src/features/Photo/pages/Main/index.jsx
@@ -3,13 +3,26 @@ import Banner from '../../../../components/Banner';
 import Images from '../../../../constants/images';
 import { Link } from 'react-router-dom';
 import { Container } from 'reactstrap';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import PhotoList from 'features/Photo/components/PhotoList';
+import { removePhoto } from 'features/Photo/photoSlice';
 
 MainPage.propTypes = {};
 
 function MainPage(props) {
   const photos = useSelector(state => state.photos);
+  const dispatch = useDispatch();
   console.log(photos);
+
+  const handlePhotoRemoveClick = (photo) => {
+    console.log('Remove:', photo);
+    const photoId = photo.id;
+    dispatch(removePhoto(photoId))
+  }
+
+  const handlePhotoEditClick = (photo) => {
+    console.log('Edit:', photo);
+  }
 
   return (
     <div className="photo-main">
@@ -17,6 +30,12 @@ function MainPage(props) {
 
       <Container className="text-center">
         <Link to="/photos/add">Add new photo</Link>
+
+        <PhotoList 
+          photoList={photos}
+          onPhotoEditClick={handlePhotoEditClick}
+          onPhotoRemoveClick={handlePhotoRemoveClick}
+        />
       </Container>
     </div>
   );
